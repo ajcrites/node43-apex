fb64d097cbb24ebd43b273057c43c1bf98138608
Zachary Drummond <zdrummond@biaprotect.com>
2016-06-08 10:37:21 -0700

    Support non DB based lambdas

--- index.js
+++ index.js
@@ -15,31 +15,59 @@ var db_connection = mysql.createConnection({
 });
 
 // much λ, much UX.
-module.exports = function db_lambda_main(fn) {
-  return function(event, context, callback) {
-    db_connection.connect()
-    try {
-      var v = fn(event, db_connection, context, callback)
+module.exports = {
+  db_lambda_main: function (fn) {
+    return function(event, context, callback) {
+      db_connection.connect()
+      try {
+        var v = fn(event, db_connection, context, callback)
 
-      if (v && typeof v.then == 'function') {
-        v.then(val => {
-          callback(null, val)
-          db_connection.end()
-        }).catch(err => {
-          console.error(err.stack)
-          sentryClient.captureException(err)
-          callback(err)
-        })
-        return
+        if (v && typeof v.then == 'function') {
+          v.then(val => {
+            callback(null, val)
+            db_connection.end()
+          }).catch(err => {
+            console.error(err.stack)
+            sentryClient.captureException(err)
+            callback(err)
+          })
+          return
+        }
+
+        // This is the non-promise logic, but should be able to remove this branch and do both the same way
+        callback(null, v)
+        db_connection.end()
+      } catch (err) {
+        console.error(err.stack)
+        sentryClient.captureException(err)
+        callback(err)
       }
+    }
+  },
 
-      // This is the non-promise logic, but should be able to remove this branch and do both the same way
-      callback(null, v)
-      db_connection.end()
-    } catch (err) {
-      console.error(err.stack)
-      sentryClient.captureException(err)
-      callback(err)
+  lambda_main: function (fn) {
+    return function(event, context, callback) {
+      try {
+        var v = fn(event, context, callback)
+
+        if (v && typeof v.then == 'function') {
+          v.then(val => {
+            callback(null, val)
+          }).catch(err => {
+            console.error(err.stack)
+            sentryClient.captureException(err)
+            callback(err)
+          })
+          return
+        }
+
+        // This is the non-promise logic, but should be able to remove this branch and do both the same way
+        callback(null, v)
+      } catch (err) {
+        console.error(err.stack)
+        sentryClient.captureException(err)
+        callback(err)
+      }
     }
   }
 }
